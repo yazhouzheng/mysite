6122f003416f48887beb827a1e4fb9390e6de0b5
asher.zheng <yazhou_zheng@163.com>
2018-04-02 22:31:26 -0700

    1. Stylize the Question admin page

--- polls/admin.py
+++ polls/admin.py
@@ -1,8 +1,25 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Choice
 
 
 # Register your models here.
 
-admin.site.register(Question)
+# class ChoiceInline(admin.StackedInline):
+class ChoiceInline(admin.TabularInline):
+    model = Choice
+    extra = 3
+
+
+class QuestionAdmin(admin.ModelAdmin):
+    # fields = ['pub_date', 'question_text']
+    fieldsets = [
+        (None,              {'fields': ['question_text']}),
+        ('Data information', {'fields': ['pub_date']}),
+    ]
+    inlines = [ChoiceInline]
+
+
+admin.site.register(Question, QuestionAdmin)
+# admin.site.register(Question)
+# admin.site.register(Choice)
